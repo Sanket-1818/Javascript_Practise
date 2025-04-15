60a4641eb85eccb428e29be30f711e511c81a6d7
Sanketh S <sanketshettyjan18@gmail.com>
2025-04-15 16:28:25 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -6,5 +6,5 @@ let p=new Promise((resolve)=>{
 p.then((result)=>{
     console.log(result)
 })
-
+//In this example,we can see the resolve related example
 //In promise "resolve" uses then method and "reject" uses catch method
